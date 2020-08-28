1886fa83a9cf36955fa3e56c841c982c60a0e04a
Luke Davies <luke@juicebox.com.au>
2020-08-28 14:12:56 +0800

    updated meta info to reflect new public folder

--- components/Meta.js
+++ components/Meta.js
@@ -4,8 +4,8 @@ const Meta = () => (
     <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <link rel="shortcut icon" href="/static/favicon.png" />
-        <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
         <title>2ND SURF</title>
         <link
             href="https://fonts.googleapis.com/css2?family=Montserrat&family=Rubik:wght@400;500;700&display=swap"
